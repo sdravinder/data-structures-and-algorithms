504c01fc19dd2416cfcf587926488982da3b2c22
Ravinder Singh <sdravindersingh1999@gmail.com>
2024-10-26 23:13:56 +0530

    intial

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,10 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("ravinder singh");
+        for(int i =0; i<=10;i++){
+            System.out.println("RS");
+
+        }
     }
 }
