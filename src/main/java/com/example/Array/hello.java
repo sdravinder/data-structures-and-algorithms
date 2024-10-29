14695d009d0f6839c52d1a4842a631f89c58ba9c
Ravinder Singh <sdravindersingh1999@gmail.com>
2024-10-29 18:18:01 +0530

    adding files

---
+++ src/main/java/com/example/Array/hello.java
@@ -0,0 +1,11 @@
+package com.example.Array;
+
+public class hello {
+    String name;
+    int age;
+
+    public hello(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
