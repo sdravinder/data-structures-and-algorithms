ef4f4e8692234bbbd9fab48450646929b7b86bd7
Ravinder Singh <sdravindersingh1999@gmail.com>
2024-10-30 17:11:43 +0530

    adding files

--- src/main/java/com/example/Array/hello.java
+++ src/main/java/com/example/Array/hello.java
@@ -3,9 +3,11 @@ package com.example.Array;
 public class hello {
     String name;
     int age;
+    int no;
 
     public hello(String name, int age) {
         this.name = name;
         this.age = age;
+        this.no = 0;
     }
 }
